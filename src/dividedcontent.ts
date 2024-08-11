01dba5936f2406dc5488a58ec4a2249c58b48307
kyaoNK <nkojima0823@gmail.com>
2024-08-11 12:06:04 +0900

    Prototype v2.

--- src/dividedcontent.ts
+++ src/dividedcontent.ts
@@ -1,8 +1,5 @@
 import * as vscode from 'vscode';
 import * as puppeteer from 'puppeteer';
-import * as fs from 'fs';
-import * as path from 'path';
-import pretty from 'pretty';
 
 interface TreeNode {
 	tag: string;
@@ -10,11 +7,6 @@ interface TreeNode {
 	height: number;
 	topOffset: number;
 	bottomOffset: number;
-	marginTop: number;
-	marginBottom: number;
-	paddingTop: number;
-    paddingBottom: number;
-	divideHere: boolean;
 	attributes?: string;
 	isHidden: boolean;
 	children: TreeNode[];
@@ -24,57 +16,29 @@ export class ContentDivider {
 
 	private readonly MAX_WIDTH : number = 1920;
 	private readonly MAX_HEIGHT: number = 1080;
-	private readonly MAX_NUM_DIVISIONS: number = 6;
-	private readonly MIN_NUM_DIVISIONS: number = 1;
-	private readonly MAX_FONT_SIZE: number = 24;
-	private readonly MIN_FONT_SIZE: number = 14;
+	private readonly MAX_NUM_COLUMNS: number = 6;
+	private readonly MIN_NUM_COLUMNS: number = 1;
+	private readonly MAX_FONTSIZE: number = 24;
+	private readonly MIN_FONTSIZE: number = 14;
 
-	private htmlString: string;
+	private htmlContent: string;
 	private cssContent: string;
 
-	private currentFontSize: number = 0;
-	private currentNumDivisions: number = 0;
-
 	private domTree: TreeNode | null = null;
-	private dividedDomTrees: TreeNode[] = [];
 
 	private browser: puppeteer.Browser | null = null;
 	private page: puppeteer.Page | null = null;
 
 	private optimalFontSize: number = 0;
-	private optimalDivisions: number = 0;
+	private optimalNumColumns: number = 0;
 
-	constructor(htmlString: string) {
-		this.htmlString = `<html><head></head><body>${htmlString}</body></html>`;
-		this.currentFontSize = this.MAX_FONT_SIZE;
-		this.cssContent = '';
+	constructor(htmlContent: string, cssContent: string) {
+		this.htmlContent = `<html><head></head><body>${htmlContent}</body></html>`;
+		this.cssContent = cssContent;
 	}
 
 	public async initialize(): Promise<void> {
-		if (this.browser) {
-			await this.close();
-		}
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No active editor found.');
-			return;
-		}
-		const document = editor.document;
-		if (document.languageId !== 'markdown') {
-			vscode.window.showErrorMessage('Active document is not a Markdown file.');
-			return;
-		}
-		const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
-		if (!workspaceFolder) {
-			vscode.window.showErrorMessage('No workspace folder found for the current document.');
-			return;
-		}
-
-		const cssPath = vscode.Uri.joinPath(workspaceFolder.uri, 'md-wallpaper', 'style.css');
-		// const cssPath = vscode.Uri.file(path.join(path.dirname(vscode.window.activeTextEditor?.document.uri.fsPath || ''), 'md-wallpaper', 'style.css'));
-		const cssContentBuffer = await vscode.workspace.fs.readFile(cssPath);
-		this.cssContent = Buffer.from(cssContentBuffer).toString('utf-8');
-
+		if (this.browser) await this.close();
 		const launchOptions: puppeteer.PuppeteerLaunchOptions = {
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
             defaultViewport: { 
@@ -83,13 +47,11 @@ export class ContentDivider {
                 deviceScaleFactor: 1 
             }
         };
-
 		if (puppeteer.default && typeof puppeteer.default.launch === 'function') {
 			(launchOptions as any).headless = 'new';
 		} else {
 			launchOptions.headless = true;
 		}
-
 		this.browser = await puppeteer.launch(launchOptions);
 		this.page = await this.browser.newPage();
 	}
@@ -102,77 +64,24 @@ export class ContentDivider {
 		}
 	}
 
-	public setHtmlString(htmlString: string) {
-		this.htmlString = htmlString;
-	}
-
 	public async run(): Promise<string> {
-		if (!this.browser || !this.page) {
-			await this.initialize();
-		}
+		if (!this.browser || !this.page) await this.initialize();
 		try {
 			await this.getOptimalSettings();
-
-			// // 最適な設定を適用したHTMLを取得
-			// const optimalHtml = await this.getOptimalHtml();
-		
-			// // HTMLをファイルとして保存
-			// const filePath = await this.saveHtmlFile(optimalHtml);
-			
-			// // 保存したファイルをブラウザで開く
-			// this.openInBrowser(filePath);
-	
-			this.divideTree();
-			// this.logResults();
-	
-			// await this.testHtml();
-	
 			return this.createHtml();
 		} finally {
 			await this.close();
 		}
 	}
 
-	// private async getOptimalHtml(): Promise<string> {
-    //     if (!this.page) {
-    //         throw new Error('Page not initialized.');
-    //     }
-    //     // 現在のページのHTMLを取得
-    //     let html = await this.page.content();
-    //     return pretty(html);
-    // }
-
-	// private async saveHtmlFile(html: string): Promise<string> {
-    //     const editor = vscode.window.activeTextEditor;
-    //     if (!editor) {
-    //         throw new Error('No active editor found.');
-    //     }
-        
-    //     const folderPath = path.dirname(editor.document.uri.fsPath);
-    //     const filePath = vscode.Uri.file(path.join(folderPath, 'out', 'optimal_content.html'));
-        
-    //     // fs.writeFileSync(filePath, html);
-	// 	await vscode.workspace.fs.writeFile(filePath, Buffer.from(html));
-    //     console.log(`Optimal HTML saved to: ${filePath}`);
-        
-    //     return filePath.fsPath;
-    // }
-
-    // private openInBrowser(filePath: string) {
-    //     vscode.env.openExternal(vscode.Uri.file(filePath));
-    // }
-
-	private async setDomTree(): Promise<void> {
-		if (!this.page) {
-			throw new Error('Page not initialized.');
-		}
-
-		const columnWidth = this.MAX_WIDTH / this.currentNumDivisions;
+	private async setDomTree(fontSize: number, numColumns: number): Promise<void> {
+		if (!this.page) throw new Error('Page not initialized.');
+	
+		const columnWidth = this.MAX_WIDTH / numColumns;
 		const bodyContentRegex = /<body[^>]*>([\s\S]*?)<\/body>/i;
-		const newBodyContent = `<body><div class="divided-content" style="width: ${this.MAX_WIDTH}px; font-size: ${this.currentFontSize}px;"><div class="content-column" style="max-width: ${columnWidth}px;">$1</div></div></body>`;
-		let modifiedHtml = this.htmlString.replace(bodyContentRegex, newBodyContent);
+		const newBodyContent = `<body>\n<div class="container" style="max-width: ${columnWidth}px; font-size: ${fontSize}px;">\n$1\n</div>\n</body>`;
+		let modifiedHtml = this.htmlContent.replace(bodyContentRegex, newBodyContent);
 
-		// CSSを<head>タグ内に挿入
 		modifiedHtml = modifiedHtml.replace('</head>', `<style>${this.cssContent}</style></head>`);
 
 		await this.page.setContent(modifiedHtml, {waitUntil: 'domcontentloaded'});
@@ -183,10 +92,7 @@ export class ContentDivider {
 			deviceScaleFactor: 1
 		});
 
-		 // スクロール位置をリセット
-		await this.page.evaluate(() => {
-            window.scrollTo(0, 0);
-        });
+		await this.page.evaluate(() => { window.scrollTo(0, 0); });
 
 		this.domTree = await this.page.evaluate(() => {
 			function getOnlyText(element: Element): string {
@@ -207,10 +113,6 @@ export class ContentDivider {
 			function createNode(element: Element): TreeNode {
 				const rect = element.getBoundingClientRect();
 				const styles = window.getComputedStyle(element);
-				const marginTop = Math.ceil(parseFloat(styles.marginTop));
-				const marginBottom = Math.ceil(parseFloat(styles.marginBottom));
-				const paddingTop = Math.ceil(parseFloat(styles.paddingTop));
-				const paddingBottom = Math.ceil(parseFloat(styles.paddingBottom));
 				const height = rect.height;
 				const topOffset = rect.top + window.scrollY;
 				const bottomOffset = rect.bottom + window.scrollY;
@@ -228,38 +130,28 @@ export class ContentDivider {
 					height: actualHeight,
 					topOffset: topOffset,
 					bottomOffset: bottomOffset,
-					marginTop: marginTop,
-					marginBottom: marginBottom,
-					paddingTop: paddingTop,
-                    paddingBottom: paddingBottom,
-					divideHere: false,
 					attributes: attributes,
 					isHidden: isHidden,
 					children: Array.from(element.children).map(createNode)
 				};
 				return node;
 			}
-			// document.body.style.fontFamily = 'Arial, sans-serif'; 
-			return createNode(document.getElementsByClassName("content-column")[0]);
+			return createNode(document.getElementsByClassName("container")[0]);
 		});
+
 	}
 
 	private async getOptimalSettings(): Promise<void> {
-		// this.logToFile("getOptimalSettings function is called");
-		let bestResult: {fontSize: number, numDivisions: number} | null = null;
-		for (let numDivisions = this.MIN_NUM_DIVISIONS; numDivisions <= this.MAX_NUM_DIVISIONS; numDivisions++) {
-			for (let fontSize = this.MAX_FONT_SIZE; fontSize >= this.MIN_FONT_SIZE; fontSize--) {
-				this.currentFontSize = fontSize;
-				this.currentNumDivisions = numDivisions;
+		let bestResult: {fontSize: number, numColumns: number} | null = null;
+		for (let numColumns = this.MIN_NUM_COLUMNS; numColumns <= this.MAX_NUM_COLUMNS; numColumns++) {
+			for (let fontSize = this.MAX_FONTSIZE; fontSize >= this.MIN_FONTSIZE; fontSize--) {
 				try {
-					await this.setDomTree();
-					const fits = this.contentFitsInDivisions(numDivisions);
+					await this.setDomTree(fontSize, numColumns);
+					const fits = this.contentFitsInColumns(numColumns);
 					if (fits) {
-						bestResult = { fontSize: fontSize, numDivisions: numDivisions };
-						// this.logToFile(`Found optimal settings: fontSize=${fontSize}, divisions=${numDivisions}`);
+						bestResult = { fontSize, numColumns };
 						this.optimalFontSize = bestResult.fontSize;
-						this.optimalDivisions = bestResult.numDivisions;
-						// this.logToFile(`\n${JSON.stringify(this.domTree, null, 4)}`);
+						this.optimalNumColumns = bestResult.numColumns;
 						return;
 					}
 				} catch (error) {
@@ -267,233 +159,65 @@ export class ContentDivider {
 				}
 			}
 		}
-		if (!bestResult) {
-			bestResult = { fontSize: this.MIN_FONT_SIZE, numDivisions: this.MAX_NUM_DIVISIONS };
-			// this.logToFile("No optimal settings found, using minimum values");
-		}
+		if (!bestResult) bestResult = { fontSize: this.MIN_FONTSIZE, numColumns: this.MAX_NUM_COLUMNS };
 		this.optimalFontSize = bestResult.fontSize;
-		this.optimalDivisions = bestResult.numDivisions;
-		// this.logToFile(`\n${JSON.stringify(this.domTree, null, 4)}`);
+		this.optimalNumColumns = bestResult.numColumns;
+		return;
 	}
 
-	private contentFitsInDivisions(divisions: number): boolean {
-		let usedDivisions = 1;			// 使用された分割数を追跡
+	private contentFitsInColumns(numColumns: number): boolean {
+		let usedColumns = 1;
 		let lastDivideOffset = 0;
-		// 新しい分割を開始する関数
-		const startNewDivision = (node: TreeNode): boolean => {
-			if (usedDivisions < divisions) {
-				node.divideHere = true;
-				usedDivisions++;
+		const startNewColumn = (node: TreeNode): boolean => {
+			if (usedColumns < numColumns) {
+				usedColumns++;
 				lastDivideOffset = node.topOffset;
-				// console.log(`========================== New Division at ${lastDivideOffset} ==========================`);
+				console.log(`================ ${node.tag} ================`);
 				return true;
 			}
 			return false;
 		};
-		// 深さ優先探索でツリーを走査する関数
+		
 		const dfs = (node: TreeNode): boolean => {
 			if (node.isHidden) {
 				return true;
 			}
-
-			// const effectiveMarginTop = calculateEffectiveMargin(node.marginTop);
-			const nodeRelativeOffset = node.topOffset - lastDivideOffset;
-			const relativeBottomOffset = node.bottomOffset - lastDivideOffset;
-
-			// 末端ノード（子を持たないノード）の場合のみ分割を考慮
+			const relativeBottomOffset = node.bottomOffset - lastDivideOffset;			
 			if (node.children.length === 0) {
-				// ノードが高さ制限を超える場合、新しい分割を開始
+				console.log(`Leaf ${node.tag} | bottomOffset: ${node.bottomOffset} | relative: ${relativeBottomOffset} | ${node.directTextContent}`);
+
 				if (relativeBottomOffset > this.MAX_HEIGHT) {
-					if (!startNewDivision(node)) {
-						return false;  // これ以上分割できない場合
+					if (!startNewColumn(node)) {
+						return false;
 					}
 				}
-				// prevMarginBottom = node.marginBottom;
-				// console.log(`Leaf ${node.tag} | Top: ${nodeRelativeOffset} | Bottom: ${relativeBottomOffset} | Height: ${node.height} | ${node.directTextContent.length > 10 ? node.directTextContent.substring(0, 10) + '...' : node.directTextContent}`);
 			} else {
-				// 内部ノードの場合、子ノードを処理
 				for (const child of node.children) {
 					if (!dfs(child)) {
 						return false;
 					}
 				}
-				// 内部ノードの処理後、高さが1080pxを超えていないか確認
 				const lastChild = node.children[node.children.length - 1];
 				const lastChildRelativeBottom = lastChild.bottomOffset - lastDivideOffset;
 				if (lastChildRelativeBottom > this.MAX_HEIGHT) {
-					if (!startNewDivision(node)) {
+					if (!startNewColumn(node)) {
 						return false;
 					}
 				}
 			}
             return true;
 		};
-		if (this.domTree === null) {
-			throw new Error('domTree is null in contentFitsInDvisions');
-		}
+		if (this.domTree === null) throw new Error('domTree is null in contentFitsInDvisions');
 		const result = dfs(this.domTree);
-		// console.log(`========== Divisions: ${divisions}. Font size: ${this.currentFontSize}. Columns used: ${usedDivisions}. ==========`);
-		return result && usedDivisions <= divisions;
-	}
-
-	// DOMツリーを分割するメインメソッド
-	private divideTree(): void {
-		// console.log("開始: divideTree()"); // デバッグ出力
-		// ---------------------------------- //
-		if (this.domTree === null) {
-			return;
-		}
-		const dividedTrees: TreeNode[] = [];	// 探索済みで分割された部分木たち
-		let exploredTree: TreeNode = {...this.domTree, children: []};	// 探索済みでコピーされた部分木
-		let unexploredTree: TreeNode = JSON.parse(JSON.stringify(this.domTree));	// 未探索の木
-		// ---------------------------------- //
-
-		// 再帰的に木構造を探索し、分割する関数
-		const traverse = (unexploredNode: TreeNode, exploredNode: TreeNode): boolean => {
-			for (let i = 0; i < unexploredNode.children.length; i++) {
-				const childNode = unexploredNode.children[i];
-				const exploredChildNode = {...childNode, children: []};
-				exploredNode.children.push(exploredChildNode);
-
-				if (childNode.divideHere) {
-					// 分割ポイントに達した場合、現在の探索済みの木を結果に追加
-					dividedTrees.push(JSON.parse(JSON.stringify(exploredTree)));
-					// console.log(`=============== ${childNode.tag}で分割しました ===============`); // デバッグ出力
-					// this.logToFile(`現在の分割結果\n${JSON.stringify(dividedTrees, null, 4)}`);
-					// this.logToFile(`現在の未探索の部分木\n${JSON.stringify(unexploredTree, null, 4)}`);
-					// 探索済みの木をリセット
-					unexploredNode.children.splice(0, i+1);
-                    exploredTree = {...this.domTree!, children: []};
-                    // exploredNode = exploredTree;
-					return true;
-				} 
-				if (traverse(childNode, exploredChildNode)) {
-					return true;
-				}
-				// 探索済みの木をリセット
-				unexploredNode.children.splice(i, 1);
-				i--;
-				// unexploredNode.children.shift();
-			}
-			unexploredNode.children = [];
-			return false;
-		};
-		while (unexploredTree.children.length > 0) {
-			traverse(unexploredTree, exploredTree);
-		}
-		
-		// 最後の部分木を追加
-		if (exploredTree.children.length > 0) {
-			dividedTrees.push(JSON.parse(JSON.stringify(exploredTree)));
-		}
-
-		// 最後の部分木を追加（未探索の木に子ノードが残っているか、分割された木が一つもない場合）
-		// this.logToFile(`未探索の部分木\n${JSON.stringify(unexploredTree, null, 4)}`);
-		// this.logToFile(`最後の探索済みの部分木\n${JSON.stringify(exploredTree, null, 4)}`);
-		this.dividedDomTrees = dividedTrees;
-		// this.logToFile(`分割結果\n${JSON.stringify(this.dividedDomTrees, null, 4)}`); // デバッグ出力
-        // console.log("終了: divideTree()"); // デバッグ出力
+		return result && usedColumns <= numColumns;
 	}
 
 	private createHtml(): string {
-		function escapeHtml(unsafe: string): string {
-			return unsafe
-					.replace(/&/g, '&amp;')
-					.replace(/</g, '&lt;')
-					.replace(/>/g, '&gt;')
-					.replace(/"/g, '&quot')
-					.replace(/'/g, '&#039;');
-		}
-		function treeToHtml(node: TreeNode) {
-			if (node.isHidden) {
-				return '';
-			}
-			let html = `<${node.tag}`;
-			if (node.attributes) {
-				html += ` ${node.attributes}`;
-			}
-			html += '>';
-			if (node.directTextContent.trim()) {
-				html += escapeHtml(node.directTextContent.trim());
-                // html += `(${node.topOffset}|${node.height}|${node.bottomOffset})`;
-                // html += `(${node.bottomOffset})`;
-			}
-			if (node.children.length > 0) {
-				html += '\n';
-				for (const child of node.children) {
-					html += treeToHtml(child);
-				}
-			}
-			html += `</${node.tag}>`;
-			return html;
-		}
-		const columnWidth = this.MAX_WIDTH / this.dividedDomTrees.length;
-		console.log(`Column Width: ${columnWidth}`);
-		let html = '';
-		html += `<div class="divided-content" style="width: ${this.MAX_WIDTH}px; font-size: ${this.optimalFontSize}px;">`;
-		for (const tree of this.dividedDomTrees) {
-			html += `<div class="content-column" style="max-width: ${columnWidth}px;">`;
-			for (const child of tree.children) {
-				html += `${treeToHtml(child)}`;
-			}
-			html += '</div>';
-		}
-		html += '</div>';
-		return html;
+		const bodyRegex = /(<body[^>]*>)([\s\S]*?)(<\/body>)/i;
+		let modifiedHtml = this.htmlContent.replace(bodyRegex, (match, openingTag, content, closingTag) => {
+			return `${openingTag}\n<div class="container" style="font-size: ${this.optimalFontSize}px; column-count: ${this.optimalNumColumns};">\n${content}</div>${closingTag}`;
+		});
+		modifiedHtml = modifiedHtml.replace('</head>', `<style>${this.cssContent}</style></head>`);
+		return modifiedHtml;
 	}
-
-	// private logResults(): void {
-		// this.logToFile('========== Optimal Settings ==========');
-		// this.logToFile(`\t\tFont Size: ${this.optimalFontSize}px`);
-		// this.logToFile(`\t\tNum Divisions: ${this.optimalDivisions}`);
-		// this.logToFile(`\t\tColumn Width: ${this.MAX_WIDTH / this.optimalDivisions}px`);
-		// this.logToFile('======================================');
-		// console.log('========== Optimal Settings ==========');
-		// console.log(`\tFont Size: ${this.optimalFontSize}px`);
-		// console.log(`\tNum Divisions: ${this.optimalDivisions}`);
-		// console.log(`\tColumn Width: ${this.MAX_WIDTH / this.optimalDivisions}px`);
-		// console.log('======================================');
-		// this.logToFile('============== DOM Tree ==============');
-		// if (this.domTree === null ) {
-		// 	throw new Error("domTree is null.");
-		// }
-		// this.logToFile(`\n${this.printTreeStructure(this.domTree)}`);
-		// this.logToFile('======================================');
-	// }
-
-	// private printTreeStructure(node: TreeNode | null = null, depth: number = 0): void {
-	// 	if (node === null) {
-	// 		return;
-	// 	}
-	// 	const indent = "\t" + "\t".repeat(depth++);
-		// this.logToFile(`${indent}<${node.tag}>`);
-		// if (node.divideHere) {
-			// this.logToFile(`${indent}divideHere: true`);
-			// console.log(`Divide here set to true for node: <${node.tag}>${node.directTextContent}</${node.tag}> in printTreeStructure.`); 
-		// }
-		// if (node.directTextContent){
-			// this.logToFile(`${indent}${node.directTextContent}`);
-		// }
-		// for (const child of node.children) {
-		// 	this.printTreeStructure(child, depth);
-		// }
-		// this.logToFile(`${indent}</${node.tag}>`);
-		// return;
-	// }
-
-	// private logToFile(message: string): void {
-	// 	const editor = vscode.window.activeTextEditor;
-	// 	if (!editor) {
-	// 		vscode.window.showErrorMessage('No active editor found.');
-	// 		return;
-	// 	}
-	// 	const logFilePath = path.join(path.dirname(editor.document.uri.fsPath), 'out', 'app.log');
-	// 	try {
-	// 		fs.mkdirSync(path.dirname(logFilePath), { recursive: true });
-	// 		fs.appendFileSync(logFilePath, `${new Date().toISOString()} - ${message}\n`);
-	// 	} catch (error) {
-	// 		console.error('Error writing to log file:', error);
-    //         vscode.window.showErrorMessage('Failed to write to log file.');
-	// 	}
-	// }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import MarkdownIt from 'markdown-it';
 import * as puppeteer from 'puppeteer';
 import pretty from 'pretty';
@@ -21,31 +20,62 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage('Active document is not a Markdown file.');
 			return;
 		}
-		const markdownText = document.getText();
-		const md = new MarkdownIt();
-		const htmlContent = md.render(markdownText);
-
-		const divider = new ContentDivider(htmlContent);
-		await divider.initialize();
-		const dividedHtml = await divider.run();
-
 		const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
 		if (!workspaceFolder) {
 			vscode.window.showErrorMessage('No workspace folder found for the current document.');
 			return;
 		}
-		const cssPath = vscode.Uri.joinPath(workspaceFolder.uri, 'md-wallpaper', 'style.css');
-		// const cssPath = vscode.Uri.file(path.join(path.dirname(vscode.window.activeTextEditor?.document.uri.fsPath || ''), 'md-wallpaper', 'style.css'));
-		// const cssContent = fs.readFileSync(cssPath, 'utf-8');
-		const cssContentBuffer = await vscode.workspace.fs.readFile(cssPath);
-		const cssContent = Buffer.from(cssContentBuffer).toString('utf-8');
-		const completeHtml = insertCSS(cssContent, dividedHtml);
 
-		const prettyHtml = pretty(completeHtml);
+		const markdownText = document.getText();
+		const md = new MarkdownIt();
+		const htmlContent = md.render(markdownText);
 
-		// if (!fs.existsSync(outDir)) {
-		// 	fs.mkdirSync(outDir);
-		// }
+		const wallpaperCssFolder = vscode.Uri.joinPath(workspaceFolder.uri, 'wallpaper-css');
+		try {
+			await vscode.workspace.fs.stat(wallpaperCssFolder);
+		} catch (error) {
+			vscode.window.showErrorMessage('wallpaper-css folder not found. Please create it in your workspace root.');
+			return;
+		}
+
+		const cssPath = vscode.Uri.joinPath(wallpaperCssFolder, 'style.css');
+		let cssContent: string = '';
+		try {
+			const cssContentBuffer = await vscode.workspace.fs.readFile(cssPath);
+			cssContent = Buffer.from(cssContentBuffer).toString('utf-8');
+		} catch (error) {
+			vscode.window.showInformationMessage('Use default css. If you want to use css, create style.css in the wallpaper-css folder.');
+		}
+
+		const divider = new ContentDivider(htmlContent, cssContent);
+		await divider.initialize();
+		const dividedHtml = await divider.run();
+		await divider.close();
+
+		let prettyHtml = pretty(dividedHtml);
+
+		const backgroundImagePath = vscode.Uri.joinPath(wallpaperCssFolder, 'background-image.png');
+		vscode.workspace.fs.stat(backgroundImagePath).then( (stat) => {
+			if (stat.type === vscode.FileType.File) {
+				const bodyRegex = /body\s*{[^}]*}/;
+				const bodyMatch = prettyHtml.match(bodyRegex);
+				if (bodyMatch) {
+					const newBody = bodyMatch[0].replace(
+						'}',
+						`\tbackground-image: url('${backgroundImagePath.fsPath}');\n}`
+					);
+					const newPrettyHtml = prettyHtml.replace(bodyRegex, newBody);
+					prettyHtml = newPrettyHtml;
+					vscode.window.showInformationMessage('Successfully added background image.');
+				} else {
+					vscode.window.showWarningMessage('Not found body selector in css.');
+				}
+			} else {
+				vscode.window.showErrorMessage('Background image file is not a file.');
+			}
+		}, (error) => {
+			vscode.window.showInformationMessage('If you want to include a background image, please put background-image.png in the wallpaper-css folder.');
+		});
 
 		const outDir = vscode.Uri.joinPath(workspaceFolder.uri, 'out');
 		try {
@@ -54,13 +84,17 @@ export function activate(context: vscode.ExtensionContext) {
 			// Directory might already exist, ignore the error
 		}
 
-		const htmlPath = vscode.Uri.joinPath(outDir, 'output.html');
-		// fs.writeFileSync(htmlPath, prettyHtml);
+		const htmlPath = vscode.Uri.joinPath(outDir, 'wallpaper.html');
 		await vscode.workspace.fs.writeFile(htmlPath, Buffer.from(prettyHtml));
 
 		const wallpaperImagePath = vscode.Uri.joinPath(outDir, 'wallpaper.png');
-		// // const imagePath = path.join(outDir.fsPath, 'output.png');
-		await generateImageFromHtml(prettyHtml, wallpaperImagePath);
+		const browser: puppeteer.Browser = await puppeteer.launch();
+		const page: puppeteer.Page = await browser.newPage();
+		await page.setViewport({ width: 1920, height: 1080 });
+		await page.setContent(prettyHtml);
+		const imageBuffer = await page.screenshot({ fullPage: true });
+		await vscode.workspace.fs.writeFile(wallpaperImagePath, imageBuffer);
+		await browser.close();
 
 		vscode.window.showInformationMessage('HTML and Image generated and saved!');
 	});
@@ -69,27 +103,3 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
-
-function insertCSS(cssContent: string, htmlContent: string): string {
-	return `
-		<html>
-			<head>
-				<style>${cssContent}</style>
-			</head>
-			<body>
-				${htmlContent}
-			</body>
-		</html>
-		`;
-}
-
-async function generateImageFromHtml( html: string, outputPath: vscode.Uri ) {
-	const browser: puppeteer.Browser = await puppeteer.launch();
-	const page: puppeteer.Page = await browser.newPage();
-	await page.setViewport({ width: 1920, height: 1080 });
-	await page.setContent(html);
-	const imageBuffer = await page.screenshot({ fullPage: true });
-	await vscode.workspace.fs.writeFile(outputPath, imageBuffer);
-	// await page.screenshot({ path: outputPath, fullPage: true });
-	await browser.close();
-}
